7799eebd341eea7889de2f7b39d3f88f1dca85bd
David Dawson <me@daviddawson.me>
2017-08-07 19:18:37 +0100

    Cleanup hot-cold replay.

--- java/src/main/groovy/io/muoncore/eventstore/TestEventStore.groovy
+++ java/src/main/groovy/io/muoncore/eventstore/TestEventStore.groovy
@@ -25,114 +25,123 @@ import java.util.concurrent.atomic.AtomicLong
 
 class TestEventStore {
 
-    public static Logger log = LoggerFactory.getLogger(TestEventStore)
+  public static Logger log = LoggerFactory.getLogger(TestEventStore)
 
-    List<Event> history = new ArrayList<>();
-    List<Broadcaster> subs = new ArrayList<>();
-    AtomicLong orderid = new AtomicLong(1)
+  List<Event> history = new ArrayList<>();
+  List<Broadcaster> subs = new ArrayList<>();
+  AtomicLong orderid = new AtomicLong(1)
 
-    void clear() {
-        history.clear()
-    }
+  void clear() {
+    history.clear()
+  }
 
-    TestEventStore(Muon muon) {
+  TestEventStore(Muon muon) {
 
-      def rs = new ReactiveStreamServer(muon)
+    def rs = new ReactiveStreamServer(muon)
 
-      rs.publishGeneratedSource("/stream", PublisherLookup.PublisherType.HOT_COLD) { ReactiveStreamSubscriptionRequest request ->
+    rs.publishGeneratedSource("/stream", PublisherLookup.PublisherType.HOT_COLD) { ReactiveStreamSubscriptionRequest request ->
 
-            def stream = request.args["stream-name"]
-            def streamType = request.args["stream-type"]
-            if (!streamType) streamType = "hot-cold"
+      def stream = request.args["stream-name"]
+      def streamType = request.args["stream-type"]
+      if (!streamType) streamType = "hot-cold"
 
-            log.info "Subscribing to $stream"
+      log.info "Subscribing to $stream"
 
-            def b = Broadcaster.<Event> create()
+      def b = Broadcaster.<Event> create()
 
-            BlockingQueue q = new LinkedBlockingQueue()
+      BlockingQueue q = new LinkedBlockingQueue()
 
-            subs << b
+      subs << b
 
-            b.map {
-                if (it instanceof EventWrapper) {
-                    return it.event
-                }
-                it
-            }.filter { it.streamName == stream }.consume {
-                q.add(it)
-            }
+      b.map {
+        if (it instanceof EventWrapper) {
+          return it.event
+        }
+        it
+      }.filter { it.streamName == stream }.consume {
+        q.add(it)
+      }
 
-            if (streamType == "cold") {
-                log.info "Requested a cold replay"
-                return Streams.from(history.findAll {
-                    matches(it.streamName, stream)
-                })
-            }
-
-            if (request.args["stream-type"] && streamType == "hot-cold") {
-                log.debug "Has requested hot-cold replay .. "
-                    history.each { q.add(it) }
-            }
-
-            new Publisher() {
-                @Override
-                void subscribe(Subscriber s) {
-                    AtomicLong itemstoprocess = new AtomicLong(0)
-
-                    boolean running = true
-                    Thread t = Thread.start {
-                        while (running) {
-                            if (itemstoprocess.get() > 0) {
-                                def next = q.take()
-                                s.onNext(next)
-                                itemstoprocess.decrementAndGet()
-                            }
-                            if (itemstoprocess.get() <= 0) {
-                                sleep(100)
-                            }
-                        }
-                    }
-
-                    s.onSubscribe(new Subscription() {
-                        @Override
-                        void request(long n) {
-                            itemstoprocess.addAndGet(n)
-                        }
-
-                        @Override
-                        void cancel() {
-                          running = false
-                        }
-                    })
-                }
-            }
-
+      if (streamType == "cold") {
+        def items = history.findAll {
+          matches(it.streamName, stream)
         }
 
-        muon.protocolStacks.registerServerProtocol(new EventServerProtocolStack({ event ->
-            log.debug "Event received " + event.event
-            try {
-                synchronized (history) {
-                    def orderId = orderid.addAndGet(1)
-                    Event ev = event.event
-                    ev.orderId = orderId
-                    ev.eventTime = System.currentTimeMillis()
-                    log.info("persisted $ev")
-                    history.add(ev);
-                    event.persisted(ev.orderId, ev.eventTime);
-                    subs.stream().forEach({ q ->
-                      q.accept(event)
-                    });
-                }
-            } catch (Exception ex) {
-                event.failed(ex.getMessage());
+        log.info "Requested a cold replay, found ${items.size()} items in the stream"
+
+        if (items) {
+          return Streams.from(items)
+        } else {
+          return Streams.empty()
+        }
+      }
+
+      if (request.args["stream-type"] && streamType == "hot-cold") {
+        log.debug "Has requested hot-cold replay .. "
+        history.findAll {
+          matches(it.streamName, stream)
+        }.each { q.add(it) }
+      }
+
+      new Publisher() {
+        @Override
+        void subscribe(Subscriber s) {
+          AtomicLong itemstoprocess = new AtomicLong(0)
+
+          boolean running = true
+          Thread t = Thread.start {
+            while (running) {
+              if (itemstoprocess.get() > 0) {
+                def next = q.take()
+                s.onNext(next)
+                itemstoprocess.decrementAndGet()
+              }
+              if (itemstoprocess.get() <= 0) {
+                sleep(100)
+              }
             }
-        }, muon.getCodecs(), muon.getDiscovery()));
+          }
+
+          s.onSubscribe(new Subscription() {
+            @Override
+            void request(long n) {
+              itemstoprocess.addAndGet(n)
+            }
+
+            @Override
+            void cancel() {
+              running = false
+            }
+          })
+        }
+      }
+
     }
 
-    def matches(name, pattern) {
-        pattern = pattern.replaceAll("\\*\\*", ".\\*")
-        name ==~ pattern
-    }
+    muon.protocolStacks.registerServerProtocol(new EventServerProtocolStack({ event ->
+      log.debug "Event received " + event.event
+      try {
+        synchronized (history) {
+          def orderId = orderid.addAndGet(1)
+          Event ev = event.event
+          ev.orderId = orderId
+          ev.eventTime = System.currentTimeMillis()
+          log.info("persisted $ev")
+          history.add(ev);
+          event.persisted(ev.orderId, ev.eventTime);
+          subs.stream().forEach({ q ->
+            q.accept(event)
+          });
+        }
+      } catch (Exception ex) {
+        event.failed(ex.getMessage());
+      }
+    }, muon.getCodecs(), muon.getDiscovery()));
+  }
+
+  def matches(name, pattern) {
+    pattern = pattern.replaceAll("\\*\\*", ".\\*")
+    name ==~ pattern
+  }
 }
 
